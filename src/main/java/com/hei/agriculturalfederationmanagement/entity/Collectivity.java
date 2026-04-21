74844d3244e5024643b8998b5ad5480747ef7585
colombefioren <colomberakotonjanahary@gmail.com>
2026-04-21 16:31:10 +0300

    feat: create status enum

---
+++ src/main/java/com/hei/agriculturalfederationmanagement/entity/Collectivity.java
@@ -0,0 +1,20 @@
+package com.hei.agriculturalfederationmanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Collectivity {
+    private Integer id;
+    private String number;
+    private String speciality;
+    private Instant creationDatetime;
+    private Status status;
+}
---
+++ src/main/java/com/hei/agriculturalfederationmanagement/entity/enums/Status.java
@@ -0,0 +1,8 @@
+package com.hei.agriculturalfederationmanagement.entity.enums;
+
+public enum Status
+{
+    PENDING,
+    APPROVED,
+    REJECTED
+}
