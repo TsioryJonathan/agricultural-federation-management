c739c28bdcd82a3a28dedb80c4c45386cff171d7
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-04-21 15:23:56 +0300

    chore: initialize spring project

---
+++ src/main/java/com/hei/agriculturalfederationmanagement/AgriculturalFederationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hei.agriculturalfederationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalFederationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriculturalFederationManagementApplication.class, args);
+    }
+
+}
