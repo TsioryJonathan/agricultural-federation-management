7368501b0d6c4f5aaed567a7f290ceeb0ecad2c8
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-04-21 20:03:27 +0300

    feat: add payment exception for invalid member inscription

--- src/main/java/com/hei/agriculturalfederationmanagement/service/MemberService.java
+++ src/main/java/com/hei/agriculturalfederationmanagement/service/MemberService.java
@@ -2,6 +2,7 @@ package com.hei.agriculturalfederationmanagement.service;
 
 import com.hei.agriculturalfederationmanagement.entity.Member;
 import com.hei.agriculturalfederationmanagement.entity.dto.CreateMember;
+import com.hei.agriculturalfederationmanagement.exception.PaymentException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,14 @@ public class MemberService {
     private final ServiceRepository repository;
 
     public List<Member> createMembers(List<CreateMember> memberList) {
+        /* Check payement and minimum 2 */
+        for(CreateMember member : memberList){
+            if(!member.isMembershipDuesPaid() || !member.isRegistrationFeePaid()){
+                throw new PaymentException(member.getFirstName() + "Either membership or registration fee not paid")
+            }
+
+        }
+
 
     }
 }
