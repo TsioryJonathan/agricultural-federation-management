28dc34d2b68b4a6ac3c6f9972554ff7242410bf7
colombefioren <colomberakotonjanahary@gmail.com>
2026-04-21 16:01:38 +0300

    feat: add get federation endpoint

--- src/main/java/com/hei/agriculturalfederationmanagement/controller/FederationController.java
+++ src/main/java/com/hei/agriculturalfederationmanagement/controller/FederationController.java
@@ -1,13 +1,27 @@
 package com.hei.agriculturalfederationmanagement.controller;
 
+import com.hei.agriculturalfederationmanagement.service.FederationService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/federation")
+@AllArgsConstructor
 public class FederationController {
 
+    private FederationService service;
+
     @GetMapping
+    private ResponseEntity<?> findFederation(){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(service.findFederation());
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Federation not found!")
+        }
+    }
 
 }
