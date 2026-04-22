7934a650c01bd08463754ea6d7d00dbf9fc2d952
colombefioren <colomberakotonjanahary@gmail.com>
2026-04-22 08:43:51 +0300

    feat: implement AssignCollectivityIdentityRequest dto'

---
+++ src/main/java/com/hei/agriculturalfederationmanagement/entity/dto/AssignCollectivityIdentityRequest.java
@@ -0,0 +1,15 @@
+package com.hei.agriculturalfederationmanagement.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AssignCollectivityIdentityRequest {
+    private String number;
+    private String name;
+}
