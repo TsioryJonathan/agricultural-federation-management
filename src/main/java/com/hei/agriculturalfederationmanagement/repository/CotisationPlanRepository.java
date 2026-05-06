c25fed6d373ab63d0a64dde933799efbefb8bd95
colombefioren <colomberakotonjanahary@gmail.com>
2026-05-06 10:29:34 +0300

    fix: types in member repository

--- src/main/java/com/hei/agriculturalfederationmanagement/repository/CotisationPlanRepository.java
+++ src/main/java/com/hei/agriculturalfederationmanagement/repository/CotisationPlanRepository.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
